89811129e8bef5e48d3ca632196e39d6c1e24db7
Brian G Castro <PrismaticQ@gmail.com>
2016-12-18 21:16:35 -0500

    significant overhauls to design of landing and about page. misc updates to map.js script

--- landing.js
+++ landing.js
@@ -1,17 +1,50 @@
-// lat_start;
-// long_start;
+imagelist = [
+    ['bkgrnd_img-0.jpg',"Perseid Meteor over Capitol Reef N.M., UT","Photo credit: Brian Castro"],
+    // ['bkgrnd_img-1.jpg',"Pleiades Star Cluster (M45)","Photo credit: Brian Castro"],
+    ['bkgrnd_img-2.jpg',"Lake Tahoe","Photo credit: Brian Castro"],
+    ['bkgrnd_img-3.jpg',"Orion over Stateline, NV","Photo credit: Brian Castro"],
+    ['bkgrnd_img-4.jpg',"Orion","Photo credit: Brian Castro"],
+    ['bkgrnd_img-5.jpg',"Elk Creek Campground, Big Sur, CA","Photo credit: Brian Castro"],
+    ['bkgrnd_img-6.jpg',"Lost Coast, CA","Photo credit: Brian Castro"],
+    ['bkgrnd_img-7.jpg',"Lost Coast, CA","Photo credit: Brian Castro"],
+    ['bkgrnd_img-8.jpg',"Shelter Cove, CA","Photo credit: Brian Castro"],
+    ['bkgrnd_img-9.jpg',"Bryce Canyon National Park, UT","Photo credit: Brian Castro"],
+    ['bkgrnd_img-10.jpg',"Arizona-Nevada Border","Photo credit: Brian Castro"],
+    ['bkgrnd_img-11.jpg',"Orion from Petaluma, CA","Photo credit: Brian Castro & Suzie Ross"],
+    ['bkgrnd_img-12.jpg',"Star Party at Bryce Canyon NP, UT","Photo credit: Brian Castro"],
+    ['bkgrnd_img-13.jpg',"Milky Way from Prescadero, CA","Photo credit: Brian Castro"],
+    ['bkgrnd_img-14.jpg',"Pigeon Point Lighthouse, Prescadero, CA","Photo credit: Brian Castro"],
+    ['bkgrnd_img-15.jpg',"Milky Way Galactic Core","Photo credit: Brian Castro"],
+    ['bkgrnd_img-16.jpg',"Andromeda Galaxy (M31)","Photo credit: Brian Castro"],
+    ['bkgrnd_img-17.jpg',"Yosemite National Park, CA","Photo credit: Brian Castro"],
+    // ['bkgrnd_img-18.jpg',"Milky Way Galaxy","Photo credit: Brian Castro"],
+]
 
-console.log("START!");
-// initialize()
+window.onload = function(){
 
+  randomImage = function(){
+    imageNameSize = imagelist.length,
+    lotteryNumber = Math.floor(Math.random()*imageNameSize),
+    console.log("lotteryNumber", lotteryNumber);
+    winnerImage = imagelist[lotteryNumber][0],
+    discription = imagelist[lotteryNumber][1],
+    credit = imagelist[lotteryNumber][2],
+    fullPath = "url('../../stargazr/images/" + winnerImage+"')";
+    imageInfo = [fullPath, discription, credit]
+    console.log(imageInfo);
+    return imageInfo
+  }
+
+  imageInfo = randomImage()
+
+  $('#banner').css('background-image', imageInfo[0]);
+  $('.wrapper.style3').css('background-image', imageInfo[0]);
+  $('#photo-description').text(imageInfo[1]);
+  $('#photo-credit').text(imageInfo[2]);
+}
 
 $(document).on('click', '#auto-location', function() {
-    // navigator.geolocation.getCurrentPosition(function(location) {
-    //   console.log(location.coords.latitude);
-    //   console.log(location.coords.longitude);
-    //   console.log(location.coords.accuracy);
-    // });
-    //
+
     var options = {
       enableHighAccuracy: true,
       timeout: 5000,
@@ -51,5 +84,4 @@ $(document).on('click', '#auto-location', function() {
       });
     };
     navigator.geolocation.getCurrentPosition(success, error, options);
-
 });
--- map.js
+++ map.js
@@ -3,13 +3,39 @@ var initialOpacity = 0.5;
 var OPACITY_MAX_PIXELS = 57; // Width of opacity control image
 
 //Small sample test of 6 sites
-var charts = [["CBACncrd",37.93333,-122.03333,"CBA Concord Observatory","A private <a href=http://www.lewcook.com/cbacal.htm>Observatory</a>. Part of the <a href=>Center for Backyard Astrophysics.",3],
+var charts = [
+    ["CBACncrd",37.93333,-122.03333,"CBA Concord Observatory","A private <a href=http://www.lewcook.com/cbacal.htm>Observatory</a>. Part of the <a href=>Center for Backyard Astrophysics.",3],
     ["PnncCmpCA",36.4899,-121.15029,"Pinnacles Campground","<a href=https://www.nps.gov/pinn/planyourvisit/camp.htm>Campground</a> at the east entrance of <a href=https://www.nps.gov/pinn/index.htm>Pinnacles National Park</a>.",10],
     ["Caltech",34.13565,-118.12627,"California Institute of Technology","<a href=https://www.caltech.edu/>Research and education institution</a> with  <a href=http://www.astro.caltech.edu/outreach/>astronomy outreach events</a>.",1],
     ["DrksByCA",38.02776,-122.96190,"Drakes Bay visitors center","Officially the <a href=https://www.nps.gov/pore/planyourvisit/visitorcenters.htm#CP_JUMP_63133>Kenneth C. Patrick Vistor Center</a>. Site of <a href=http://www.ptreyes.org/camps-classes-programs/field-institute/class-topics/outdoor-activities>astronomy classes</a> at the  <a href=https://www.nps.gov/pore/index.htm>Point Reyes National Seashore</a> national park. ",9],
     ["CSnzScrObCA",32.99275,-117.01099,"Hawks Landing Observatory","A private observatory",4],
-    ["PscdrCA",37.2552,-122.383,"Pescadero","",7]];
+    ["PscdrCA",37.2552,-122.383,"Pescadero","",7]
+  ];
 
+    // JQUERY ME!!!!!!!!!!
+    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+    function openNav() {
+        // document.getElementById("mySidenav").style.width = "300px";
+        $("#mySidenav").width(300);
+        // document.getElementById("main").style.marginLeft = "300px";
+        $("#main").css('margin-left','300px');
+        main_width = $("#main").width();
+        var sidebary_width = $("#mySidenav").width();
+        $("#main").width(main_width - sidebary_width);
+        resize_map()
+    }
+
+    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+        // document.getElementById("mySidenav").style.width = "0";
+        // document.getElementById("main").style.marginLeft = "0";
+        $("#mySidenav").width(0);
+        $("#main").css('margin-left','0px');
+        $("#main").width("100%");
+        resize_map()
+    }
+
+//Find closesr Clear Sky Chart Station to location selected
 function getClosestStation (lat, lng) {
   var closestStation = '',
       diff = 1000000;
@@ -21,14 +47,28 @@ function getClosestStation (lat, lng) {
     var chartDiff = Math.abs(chartLat - lat) + Math.abs(chartLng - lng);
 
     if (chartDiff < diff) {
+      console.log("Dist", chartDiff);
       diff = chartDiff;
       closestStation = chart[0]
     }
   });
-
   return closestStation;
 }
 
+function resize_map(){
+  page_height = $("#main").outerHeight();
+  data_height = $("#datadisplay").outerHeight();
+  map_height = page_height - data_height;
+  $("#map").height(map_height);
+
+  body_width = $("body").outerWidth();
+  sidebar_width = $("#mySidenav").outerWidth();
+  map_width = body_width - sidebar_width;
+  console.log("pw", body_width,  " - sbw", sidebar_width);
+  $("#map").width(map_width);
+}
+
+//Get Clear Sky Chart image, load onto page w/ link to full cleardarksky page
 function appendCskChart(station) {
   var domain = 'http://www.cleardarksky.com/c/',
       anchorAction = station + 'key.html?1',
@@ -40,64 +80,41 @@ function appendCskChart(station) {
   $('img#cskChart').attr('src', imgUri);
 }
 
-function HomeControl(controlDiv, map) {
-  // Set CSS styles for the DIV containing the control
-  // Setting padding to 5 px will offset the control
-  // from the edge of the map
-  //  controlDiv.style.padding = '5px';
-  // // Set CSS for the control border
-  // var controlUI = document.createElement('div');
-  // controlUI.style.backgroundColor = 'white';
-  // controlUI.style.borderStyle = 'solid';
-  // controlUI.style.borderWidth = '2px';
-  // controlUI.style.cursor = 'pointer';
-  // controlUI.style.textAlign = 'center';
-  // controlUI.title = 'Click for more Information';
-  // controlDiv.appendChild(controlUI);
-  // // Set CSS for the control interior
-  // var controlText = document.createElement('div');
-  // controlText.style.fontFamily = 'Arial,sans-serif';
-  // controlText.style.fontSize = '14px';
-  // controlText.style.color = 'blue';
-  // controlText.style.paddingLeft = '4px';
-  // controlText.style.paddingRight = '4px';
-  // controlText.innerHTML = '<b>More Information</b>';
-  // controlUI.appendChild(controlText);
-  // Setup the click event listeners
-  // google.maps.event.addDomListener(controlUI, 'click', function() {
-  //   var url = "http://djlorenz.github.io/astronomy/lp2006/";
-  //   window.open(url);
-  // });
-}
-
 lat_selected = 0
 lon_selected = 0
-lat_start = localStorage.getItem("lat_start")//= 37.790319 //SF: 37.790319
-lon_start = localStorage.getItem("lon_start")//= -122.40014 //SF: -122.40014
+lat_start = localStorage.getItem("lat_start") //Default to SF: 37.790319
+lon_start = localStorage.getItem("lon_start") //Default to SF: -122.40014
 zoom_start = 3
 
+var markersArray = [];
+
 function initialize() {
-            //MAKE SURE TO PUT UP DISCPLAMER THAT WIP AND NOT YET RELIABLE
-            //MAKE SURE TO PUT UP DISCPLAMER THAT WIP AND NOT YET RELIABLE
-            //MAKE SURE TO PUT UP DISCPLAMER THAT WIP AND NOT YET RELIABLE
-            //MAKE SURE TO PUT UP DISCPLAMER THAT WIP AND NOT YET RELIABLE
-            //MAKE SURE TO PUT UP DISCPLAMER THAT WIP AND NOT YET RELIABLE
+
+
+    //Sets start location to Geolocated user position from landing page
     if (lat_start){
       console.log('Geolocated Lat: ' + lat_start);
       console.log('Geolocated Lon: ' + lon_start);
+      $("#start_coordinates").text(lat_start + ", " + lon_start);
+      $("#start_coordinates").select();
     }
     else{
       console.log('Could Not Find Location! Defaulting to Downtown San Francisco!');
-      lat_start = 37.790319;
-      lon_start = -122.40014;
+      lat_start = 37.790854;
+      lon_start = -122.401315;
+      $("#start_coordinates").text(lat_start + ", " + lon_start);
+      $("#start_coordinates").select();
     }
 
+    resize_map()
+    $(window).resize(resize_map);
 
     var mapOptions = {
       zoom: zoom_start,
       maxZoom: 17,
       center: new google.maps.LatLng(lat_start, lon_start),
       scaleControl: true,
+      streetViewControl: false,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
@@ -109,8 +126,14 @@ function initialize() {
     moonPhase = 0;
     lightPol = 0;
     site_quality = 0;
+    elevation = 0;
 
-    //On mouse click:
+    function short_coord(x) {
+      //Returns Lat/Long to 5 sigfigs (~10m for Longitude, Varies for Lat)
+      return Math.round(x * 10000) / 10000;
+    }
+
+    //On map click, find lat-long and prepare get request urls
     google.maps.event.addListener(map, 'click', function(event) {
         //Get lat/long at point
         lat_selected = event.latLng.lat()
@@ -119,23 +142,85 @@ function initialize() {
         console.log("LAT:"+lat_selected);
         console.log("LNG:"+lon_selected);
 
-        $("#coordinate").val(event.latLng.lat() + ", " + event.latLng.lng());
-        $("#coordinate").select();
+        $("#selectedloc").val("SELECTED COORDINATES: "+ short_coord(event.latLng.lat()) + ", " + short_coord(event.latLng.lng()));
+        $("#selectedloc").select();
+        console.log("what is here:", $("#selectedloc").val());
 
-        // elevation_url = "https://maps.googleapis.com/maps/api/elevation/json?locations="+lat_selected+","+lon_selected"&key=AIzaSyAPV8hWJYamUd7TCnC6h6YcljuXnFW1lp8"
+        elevation_url = "http://cors.io/?https://maps.googleapis.com/maps/api/elevation/json?locations="+lat_selected+","+lon_selected+"&key=AIzaSyAPV8hWJYamUd7TCnC6h6YcljuXnFW1lp8"
         darksky_url = "http://stargazr.us-west-2.elasticbeanstalk.com/weather?lat="+lat_selected+"&lng="+lon_selected;
 	      lightpol_url = "http://stargazr.us-west-2.elasticbeanstalk.com/brightness?lat="+lat_selected+"&lng="+lon_selected;
     });
 
+    $("#set-user-location").click(function() {
+
+      $("#start_coordinates").val("Your Location: " + short_coord(lat_selected) + ", " + short_coord(lon_selected));
+      $("#start_coordinates").select();
+
+      lat_start = lat_selected;
+      lon_start = lon_selected;
+
+      // var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+      // var beachMarker = new google.maps.Marker({
+      //   position: {lat: lat_start, lng: lon_start},
+      //   map: map,
+      //   icon: image
+      // });
+      //
+      // // Sets the map on all markers in the array.
+      //       function setMapOnAll(map) {
+      //         for (var i = 0; i < markers.length; i++) {
+      //           markers[i].setMap(map);
+      //         }
+      //       }
+    });
+
+
     //on button press to get conditions at a site
     $( "#get-conditions-here" ).click(function() {
-        //
-        // $.get(elevation_url, function(elevationdata) {
-        //   
-        //
-        // }
+
+      // console.log("URL2:", elevation_url);
+
+        $("#dest_coordinates").val("Site Location: " + short_coord(lat_selected) + ", " + short_coord(lon_selected));
+        $("#dest_coordinates").select();
+
+        if ($("#mySidenav").width() === 0){
+            openNav()
+        }
+
+        $.get(elevation_url, function(elevationdata) {
+            console.log("SITE ELEVATION DATA:");
+            console.log(elevationdata);
+            elevjson = JSON.parse(elevationdata);
+            elevation = elevjson.results[0].elevation;
+            minElevation = -450; //Dead Sea
+            maxElevation = 8850; //Everest
+
+            if (elevation < -450){ //below lowest dry land on earth
+              elevation = 0; //user clicked on ocean. Give sea level, not sea floor.
+            }
+
+            //Find the percentage of atmosphere above you (up to max of 9000m)
+            //This is integral of atmospheric density as a function of hight, with an offset and scale factor
+            //y = 10.2307542413*exp(-0.000130593 x ) - 3.08
+            //Integrant: -78340.8 e^(-0.000130593 x) - 3.08 x
+            //Double check your math there
+            lower_bound = -78340.8*Math.exp(-0.000130593*minElevation)-3.08*minElevation;
+            selected_elev_val = -78340.8*Math.exp(-0.000130593*elevation)-3.08*elevation;
+            upper_bound = -78340.8*Math.exp(-0.000130593*maxElevation)-3.08*maxElevation;
+
+            area_under = selected_elev_val - lower_bound;
+
+            percent_above = 1 - area_under/(upper_bound-lower_bound);
+
+            console.log(area_under, upper_bound, selected_elev_val, lower_bound);
+            console.log(percent_above);
+
+        });
 
         $.get(darksky_url, function(weatherdata) {
+
+            console.log("SITE WEATHER REPORT:");
+            console.log(weatherdata);
 
             //set vars for weather data from request
             var precipProbability = weatherdata.currently.precipProbability
@@ -144,12 +229,20 @@ function initialize() {
             var cloudCover = weatherdata.currently.cloudCover
             var moonPhase = weatherdata.daily.data[0].moonPhase //0 tells to grab todays phase. allows 0-7
 
+            if (moonPhase > 0.5){
+              moonPhase = (1-moonPhase)/0.5
+            }
+            else{
+              moonPhase = moonPhase/0.5
+            }
+
             //Populate HTML fields
             $("#cloudCover").html( Math.round(100*cloudCover) + "%");
-            $("#visibility").html(visibility);
+            // $("#visibility").html(visibility);
+            $("#elevation").html(Math.round(elevation) + " m");
             $("#humidity").html(Math.round(100*humidity) + "%");
             $("#precipProbability").html(100*Math.round(precipProbability) + "%");
-            $("#moonPhase").html(Math.round(100*moonPhase) + "% Full")
+            $("#moonPhase").html(Math.round(100*moonPhase) + "% Full");
 
             $.get(lightpol_url, function(lightpol) {
                 lightPol = lightpol.brightness
@@ -174,10 +267,14 @@ function initialize() {
           // site_quality = (precip_quality * humid_quality * cloud_quality)*100 // to or not to include * (1-moonPhase)
           site_quality = ((((precip_quality * lightpol_quality * cloud_quality)*8) + (humid_quality*2))/10)*100
           console.log("qual", site_quality);
+
           //Determine Site quality description
           site_quality_discript = ""
-          if (site_quality > 90){
+          if (site_quality > 95){
             site_quality_discript = "Excellent"
+          }
+          else if (site_quality > 90){
+            site_quality_discript = "Very Good"
           }
           else if (site_quality > 80) {
             site_quality_discript = "Good"
@@ -209,7 +306,6 @@ function initialize() {
             destinations: lat_selected+","+lon_selected
           },
           function(dist_data){
-
             try {
               //Assign vars for time and Distance
               drive_time = dist_data.rows[0].elements[0].duration.text;
@@ -219,13 +315,12 @@ function initialize() {
               $("#ddist").html(drive_dist);
             }
             catch(err) {
-              //if no route foundm, give N/A message
+              //if no route found, give N/A message
               $("#dtime").html("N/A");
               $("#ddist").html("N/A");
             }
           }
         )
-
         appendCskChart(getClosestStation(lat_selected, lon_selected));
     });
 
@@ -491,7 +586,7 @@ function initialize() {
   createOpacityControl(map);
   map.overlayMapTypes.insertAt(0, overlayfull);
   var homeControlDiv = document.createElement('div');
-  var homeControl = new HomeControl(homeControlDiv, map);
+  // var homeControl = new HomeControl(homeControlDiv, map);
   homeControlDiv.index = 1;
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(homeControlDiv);
 }
